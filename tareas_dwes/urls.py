dce3792b5deedcc25ca7b415020472e9b003b4e1
SantyDev31 <santydelgodev@gmail.com>
2025-11-26 18:26:43 +0100

    Vistas Funcionando

--- tareas_dwes/urls.py
+++ tareas_dwes/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from tareas.views import detalle_tarea
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('tasks/<int:pk>/detail', detalle_tarea.as_view())
 ]
